559cc1e22ea14674820b972f0c1f1eca1d3b511e
ericmp <eric.peralli@yahoo.com>
2015-04-01 12:26:32 -0400

    Main
    Main project upload

---
+++ GroupProject2/GroupProject2/LockFreeQueue.cpp
@@ -0,0 +1,23 @@
+include <iostream>
+#include <atomic>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+class LockFreeQueue
+{
+public:
+	LockFreeQueue();
+	~LockFreeQueue();
+
+private:
+
+};
+
+LockFreeQueue::LockFreeQueue()
+{
+}
+
+LockFreeQueue::~LockFreeQueue()
+{
+}
