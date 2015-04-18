827ca798e7b0322fdd1ecd41852c480e762b9dc0
Ryan Villaflores <disisnayr@Live.com>
2015-04-18 15:03:57 -0400

    j
    hjkh

---
+++ GroupProject2/GroupProject2/Project.cpp
@@ -0,0 +1,34 @@
+#include "LockFreeQueue.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <thread>
+#include <iomanip>
+
+
+
+int main()
+{
+	LockFreeQueue eric;
+	eric.Enqueue(5);
+	std::cout << eric.tail.load()->value;
+	//eric.Enqueue(7);
+
+	/*
+	int b = 2;
+	int* a = &b;;
+	std::cout << &b << std::endl;
+	std::cout << a << std::endl;
+	std::cout << &a << std::endl;
+	*(++a);
+	std::cout << &a << std::endl;
+	std::cout << a << std::endl;
+	*/
+	//int a = eric.Dequeue();
+
+	//if (eric.sentinel.load().next == NULL)
+	//	std::cout << eric.sentinel.load().value;
+	//eric.Dequeue();
+	getchar();
+	return 0;
+}
